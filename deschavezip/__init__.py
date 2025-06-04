cdbf7b60a0cc6bf8a7eb00e89047271838662794
Prof-Karan-Luciano <karan.silva@ifro.edu.br>
2025-06-04 19:55:40 -0400

    Ajuste

--- deschavezip/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-__version__ = "0.1.0"
-__author__ = "Karan Luciano"
-__description__ = "Ferramenta para quebra de senhas de arquivos ZIP com interface GNOME moderna" 
